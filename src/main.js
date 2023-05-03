f92cefd918ad76e3079e007fd8eb1fd46ee6d956
Wendy <wchen188@ucsc.edu>
2023-05-03 14:21:42 -0700

    added mod desc

--- src/main.js
+++ src/main.js
@@ -1,3 +1,23 @@
+// Wendy Chen
+// Rocket Patrol Fighter
+// 10 hours
+/* Mods 
+- Speed increase after 30 secs (5)
+- Add your own (copyright-free) background music to the Play scene (please be mindful of the volume) (5)
+- Allow the player to control the Rocket after it's fired (5)
+- Track a high score that persists across scenes and display it in the UI (5)
+- Implement the 'FIRE' UI text from the original game (5)
+- Create a new scrolling tile sprite for the background (5)
+- Display the time remaining (in seconds) on the screen (10)
+- Create a new title screen (e.g., new artwork, typography, layout) (10)
+- Create 4 new explosion sound effects and randomize which one plays on impact (10)
+- Implement a new timing/scoring mechanism that adds time to the clock for successful hits (15)
+- Create a new enemy Spaceship type (w/ new artwork) that's smaller, moves faster, and is worth more points (15)
+- Implement mouse control for player movement and mouse click to fire (15)
+*/
+// Cite: https://www.pixilart.com/draw 
+// https://pixabay.com/music/search/game%20music/
+
 let config = {
     type: Phaser.AUTO,
     width: 640,
